775070f11c39c3f5fb01941e50138c64c6732199
Moisés Chiaretto <84775466+moiseschiaretto@users.noreply.github.com>
2024-06-03 16:20:59 -0300

    Delete api_rest/src/test/java/com/automation/web/api directory

--- api_rest/src/test/java/com/automation/web/api/APIConfig.java
+++
@@ -1,9 +0,0 @@
-package com.automation.api;
-
-public class APIConfig {
-	public static final String BASE_URI = "https://reqres.in/api";
-    public static final String ENDPOINT_USERS = "/users/";
-    public static final String AUTH_TOKEN = "Bearer b7e0f9be923eabf55779250a1266ee97af8c9a99adc308e0d1e5d920ba4d96aa";
-    public static final long EXPECTED_RESPONSE_TIME = 2000;
-    public static final int USER_ID = 1;
-}
