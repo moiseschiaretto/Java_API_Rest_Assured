267f8e3fb180de864bafde1614bd9b66b6e7c63b
Moisés Chiaretto <84775466+moiseschiaretto@users.noreply.github.com>
2024-06-19 14:23:53 -0300

    Update APIConfig.java

--- api_rest/src/test/java/com/automation/api/APIConfig.java
+++ api_rest/src/test/java/com/automation/api/APIConfig.java
@@ -4,7 +4,13 @@ public class APIConfig {
     // Documentação da API no site:   https://reqres.in 
     public static final String BASE_URI = "https://reqres.in/api";
     public static final String ENDPOINT_USERS = "/users/";
-    public static final String AUTH_TOKEN = "Bearer b7e0f9be923eabf55779250a1266ee97af8c9a99adc308e0d1e5d920ba4d96aa";
+    public static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
+    /* 
+     * Criar a Variável de Ambiente no Windows
+     * setx AUTH_TOKEN "valor_do_token"
+     * Remover a Variável de Ambiente no Windows
+     * setx AUTH_TOKEN "" 
+     */
     public static final long EXPECTED_RESPONSE_TIME = 2000;
     public static final int USER_ID = 1;
 }
